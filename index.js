106f7fd35924e6bcb5d2140205978449aeb98019
Ricard Torres <torres.rick@gmail.com>
2022-02-23 11:44:49 +0100

    Add dynamic options and styling

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const fs = require('fs');
+import slugify from '@sindresorhus/slugify';
+import * as fs from 'fs';
+
 
 // Create the requried folders
 fs.mkdir(`./public`, () => {});
@@ -11,4 +13,25 @@ function createFile(fileName, data) {
   });
 }
 
-createFile('./public/index.html', 'Hello World')
+function readFile(path) {
+  return fs.readFileSync(path, { encoding:'utf8', flag:'r'});
+}
+
+function _generateOptions() {
+  const shows = JSON.parse(readFile('data.json')).shows;
+  const options = shows.map((show) => `<option value="${slugify(show)}">${show}</option>`).join('\n');
+
+  return options;
+}
+
+function _init() {
+  let template = readFile('src/template.html');
+  const options = _generateOptions();
+
+  template = template.replace('%OPTIONS%', options);
+
+  createFile('./public/index.html', template);
+}
+
+_init();
+
