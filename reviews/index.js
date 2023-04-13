6a9aa0c4cc6e27c584aa349a3fdc1783a49ff3e8
Ricard Torres <torres.rick@gmail.com>
2023-04-13 21:03:30 +0900

    Manga review generator

--- reviews/index.js
+++ reviews/index.js
@@ -3,10 +3,16 @@ import { createFile, readFile } from '../utils.js';
 
 
 function _generateOptions() {
-  const shows = JSON.parse(readFile('./reviews/data.json')).shows;
-  const options = shows.map((show) => `<option value="${slugify(show)}">${show}</option>`).join('\n');
+  const data = JSON.parse(readFile('./reviews/data.json'));
 
-  return options;
+  function _options(series, type) {
+    return series.map((item) => `<option data-type="${type}" value="${slugify(item)}">${item}</option>`).join('\n')
+  }
+
+  const shows = `<optgroup label="TV Shows">${_options(data.shows, 'tv-shows')}</optgroup>`;
+  const manga = `<optgroup label="Manga">${_options(data.manga, 'manga')}</optgroup>`;
+
+  return shows + manga;
 }
 
 function _init() {
--- reviews/src/main.js
+++ reviews/src/main.js
@@ -13,35 +13,58 @@
    */
   function _markdown(config) {
     const {
+      type,
       showFormatted,
       season,
       episode,
+      volume,
       date,
       rating,
       comment,
       share
     } = config
 
+    function episodeOrVolume() {
+      let text = '';
+
+      if (+season) {
+        text += `season: ${+season}`
+      }
+
+      if (+episode) {
+        text += `\nepisode: ${+episode}`
+      }
+
+      if (+volume) {
+        text += `volume: ${+volume}`
+      }
+
+      return text
+    }
+
     return `
 ---
-type: series
+type: ${type === 'manga' ? 'manga' : 'series'}
 title: "${showFormatted} ${season}x${episode}"
 date: "${date}"
 name: "${showFormatted}"
-season: ${+season}
-episode: ${+episode}
+${episodeOrVolume()}
 rating: ${+rating}
 ratingEmoji: ${'⭐️'.repeat(+rating)}
 share: ${share === 'true' ? 'true' : 'false'}
 ---
 
-${comment ? comment : '*[No review was written for this episode]*'}
+${comment ? comment : `*[No review was written for this ${type === 'manga' ? 'volume' : 'episode'}]*`}
 `.trim();
   }
 
 
   function _setElements() {
     _$.form = document.querySelector('form');
+    _$.show = document.querySelector("[name='show']");
+    _$.seasonWrapper = document.querySelector('#season-wrapper');
+    _$.episodeWrapper = document.querySelector('#episode-wrapper');
+    _$.volumeWrapper = document.querySelector('#volume-wrapper');
   }
 
   function _formatDate() {
@@ -63,22 +86,27 @@ ${comment ? comment : '*[No review was written for this episode]*'}
   }
 
   function _handleSubmit(event) {
-    let URL = 'https://github.com/quicoto/reviews/new/master/content/blog/tv-shows/?filename=tv-shows/%SHOW%/%SEASON%x%EPISODE%/index.md';
-
     event.preventDefault();
 
     const formData = new FormData(event.target);
     const show = formData.get('show');
-    const showFormatted = document.querySelector("[name='show']").selectedOptions[0].innerText;
+    const selectedShow = document.querySelector("[name='show']").selectedOptions[0]
+    const commonPath = "https://github.com/quicoto/reviews/new/master/content/blog/"
+    const folder = selectedShow.dataset.type === 'manga' ? '%VOLUME%' : '%SEASON%x%EPISODE%';
+    let URL = `${commonPath}${selectedShow.dataset.type}/?filename=${selectedShow.dataset.type}/%SHOW%/${folder}/index.md`;
+    const showFormatted = selectedShow.innerText;
     const season = formData.get('season');
     const episode = formData.get('episode');
+    const volume = formData.get('volume');
     const rating = formData.get('rating');
     const comment = formData.get('comment');
     const share = formData.get('share');
     const content = _markdown({
       showFormatted,
+      type: selectedShow.dataset.type,
       season,
       episode,
+      volume,
       date: _formatDate(),
       rating,
       comment,
@@ -88,16 +116,31 @@ ${comment ? comment : '*[No review was written for this episode]*'}
     URL = URL.replace('%SHOW%', show);
     URL = URL.replace('%SEASON%', season);
     URL = URL.replace('%EPISODE%', episode);
+    URL = URL.replace('%VOLUME%', volume);
 
     navigator.clipboard.writeText(content);
 
     window.location.href = URL;
   }
 
-  function _addEventListeners() {
-    _$.form.addEventListener('submit', _handleSubmit);
+  function _handleChange() {
+    const show = document.querySelector("[name='show']").selectedOptions[0];
+
+    if (show.dataset.type === 'manga') {
+      _$.seasonWrapper.setAttribute('hidden', true);
+      _$.episodeWrapper.setAttribute('hidden', true);
+      _$.volumeWrapper.removeAttribute('hidden');
+    } else if (show.dataset.type === 'tv-shows') {
+      _$.seasonWrapper.removeAttribute('hidden');
+      _$.episodeWrapper.removeAttribute('hidden');
+      _$.volumeWrapper.setAttribute('hidden', true);
+    }
   }
 
+  function _addEventListeners() {
+    _$.form.addEventListener('submit', _handleSubmit);
+    _$.show.addEventListener('change', _handleChange);
+  }
 
   function _init() {
     _setElements();
